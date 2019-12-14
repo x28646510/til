b5207e3f76bf5c2edecae277106341e4266a454b
x28646510 <x28646510@gmail.com>
2019-12-14 13:34:39 +0900

    julia

---
+++ til-julia/README.md
@@ -0,0 +1,7 @@
+# TIL Julia
+
+## Done
+
+- Install Julia
+- Install Atom
+  - Install Juno IDE
---
+++ til-julia/practice.jl
@@ -0,0 +1,42 @@
+1 + 2
+
+x = 10
+x + 1
+x = 1 + 1
+x = "Hello, World!"
+
+pi
+
+typeof(1)
+
+Sys.WORD_SIZE
+
+Int
+
+UInt
+
+0x1
+
+0b10
+
+0o010
+
+(typemin(Int32), typemax(Int32))
+
+1.0
+
+5 % 2
+
+x = 3
+
+2x
+
+[1,2,3] .+ 3
+
+'x'
+
+typeof('x')
+
+greet = "Hello"
+whom = "world"
+"$greet, $whom.\n"
